3a74dcec81eae383c3653cd6eb02415126e59fa9
JIM HALLOCK <justjim1220@gmail.com>
2019-02-05 17:16:47 -0600

    Update MMM-KitchenTimer.js

--- MMM-KitchenTimer.js
+++ MMM-KitchenTimer.js
@@ -29,7 +29,7 @@ Module.register("MMM-KitchenTimer", {
 
 	// Define required scripts.
 	getScripts: function() {
-		return ["jquery.js"];
+		return ["https://code.jquery.com/jquery-3.3.1.js"];
 	},
 
 	// Define styles.
